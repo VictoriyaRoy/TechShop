3b31fc70982e6e8d1004b6526d096827357233bc
VictoriyaRoy <victoriyaroi2003@gmail.com>
2022-05-24 19:07:32 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import ShoppingCart from './components/ShoppingCart'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Footer/> 
     </div>
   );
 }
